d52e97dfc525cd5cb8680d2841be2b4c88c5b643
User2 <Vika-hd@mail.ru>
2022-07-02 14:42:11 +0300

    Add first test

---
+++ src/test/java/com/viktoriya/test/UiTest.java
@@ -0,0 +1,31 @@
+package com.viktoriya.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UiTest {
+    @Test
+    public void test() {
+        Assertions.assertEquals("viktoriya", "viktoriya");
+    }
+
+    @Test
+    public void test1() {
+        Assertions.assertEquals("viktoriya", "viktoriya");
+    }
+
+    @Test
+    public void test2() {
+        Assertions.assertEquals("viktoriya", "viktoriya");
+    }
+
+    @Test
+    public void test3() {
+        Assertions.assertEquals("viktoriya", "viktoriya");
+    }
+
+    @Test
+    public void test4() {
+        Assertions.assertEquals("viktoriya", "viktoriya");
+    }
+}
